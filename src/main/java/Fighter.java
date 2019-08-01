595357dcf8729667ef0afd610d3c39f4d877ec52
Steph Scully <stephanielarascullion@yahoo.com>
2019-08-01 11:43:12 +0100

    add Fighter abstract class, subclass of Player

---
+++ src/main/java/Fighter.java
@@ -0,0 +1,19 @@
+public abstract class Fighter extends Player {
+
+    private PlayerAttribute weapon;
+
+    public Fighter(String name, int healthPoints, PlayerAttribute weapon) {
+        super(name, healthPoints);
+        this.weapon = weapon;
+    }
+
+    public PlayerAttribute getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(PlayerAttribute weapon) {
+        this.weapon = weapon;
+    }
+
+    abstract public int attack();
+}
